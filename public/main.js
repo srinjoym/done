0a873ef8daec52a7852f03409271a8981af41a19
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2019-09-13 00:52:06 -0500

    convert to menu bar app

--- public/main.js
+++ public/main.js
@@ -1,17 +1,18 @@
-const electron = require("electron")
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow, nativeImage, Tray} = require("electron")
 const path = require("path")
 const url = require("url")
 const isDev = require("electron-is-dev")
 
-let mainWindow
+let mainWindow = null
+let tray = null
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
-    width: 400,
-    height: 750,
-    minHeight: 300,
-    minWidth: 300,
+    width: 300,
+    height: 350,
+    show: false,
+    frame: false,
+    resizable: false,
     webPreferences: {
       nodeIntegration: true
     }
@@ -20,7 +21,46 @@ const createWindow = () => {
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
 }
 
+const setupMenubar = () => {
+  const icon = nativeImage.createFromPath(path.join(__dirname, './icon.png'))
+  tray = new Tray(icon)
+
+  // Add a click handler so that when the user clicks on the menubar icon, it shows
+  // our popup window
+  tray.on('click', function(event) {
+    toggleWindow()
+  })
+}
+
+const toggleWindow = () => {
+  if (mainWindow.isVisible()) {
+    mainWindow.hide()
+  } else {
+    showWindow()
+  }
+}
+
+const showWindow = () => {
+  const trayPos = tray.getBounds()
+  const windowPos = mainWindow.getBounds()
+  let x, y = 0
+  if (process.platform === 'darwin') {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height)
+  } else {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height * 10)
+  }
+
+
+  mainWindow.setPosition(x, y, false)
+  mainWindow.show()
+  mainWindow.focus()
+}
+
+
 app.on("ready", () => {
+  setupMenubar()
   createWindow()
 })
 
